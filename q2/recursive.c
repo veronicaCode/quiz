09b1695f54f87c8542ddf4d98933b219b6d95221
Veronica Chiang <veronica82915@gmail.com>
2015-10-13 09:43:41 -0400

    add recursive version
    data input is the same as iterative version, I haven't test the boundary case.

--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,29 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <string.h>
+
+char find_miniest_ch (char *s, char c, int p)
+{
+    if ((*(s + p)) == '\0'){
+        return (*s);
+    }
+    if ((*s+p) == c) {
+        if (*(s+p+1) != '\0') 
+            return *(s+1);
+    }
+    else {
+        p++;
+        return find_miniest_ch(s, c, p);
+    }
+}
 
 int main()
 {
+    char str[30];
+    char c;
+    gets(str);
+    str[strlen(str)] = '\0';
+    c = getchar();
+    c = find_miniest_ch(&str, c, 0);
+    putchar(c);
     return 0;
 }
