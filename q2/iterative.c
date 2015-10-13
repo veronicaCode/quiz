2dcf8da46db30e5e9da012b728c6365bcf135ac8
Veronica Chiang <veronica82915@gmail.com>
2015-10-13 08:12:22 -0400

    find the miniest character in iterative version
    execute the program, first enter the string press enter to key the character want to find in the string

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,21 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <string.h>
 
 int main()
 {
+    char str[30];
+    char c;
+    gets(str);
+    c = getchar();
+    int size = strlen(str);
+    if (!size)
+        return 0;
+    for (int i = 0; i < size - 1; i++) {
+        if (str[i] == c) {
+            putchar(str[i + 1]);
+            return 0;
+        }
+    }
+    putchar(str[0]);
     return 0;
 }
