ea9bf3c1326ed8f2a9e10ac40405bdce63dd94eb
Veronica Chiang <veronica82915@gmail.com>
2015-10-14 07:55:30 -0400

    Find maxsubarray iterative version done.
    It will begin at the first number n, and the following n numbers are the array item. If want to end the program, let n = 0.

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,37 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+
+int maxSubArray(int A[], int n)
+{
+    int max_so_far = 0;
+    int max_end_here = 0;
+    int i = 0;
+    for (i = 0; i < n; i++) {
+        max_end_here = A[i] + max_end_here;
+        if (max_end_here < 0)
+            max_end_here = 0;
+        if (max_so_far < max_end_here)
+            max_so_far = max_end_here;
+    }
+    return max_so_far;
+}
 
 int main()
 {
+    int count = 0, i = 0;
+    int *A;
+    while (1) {
+        scanf("%d", &count);
+        if (count == 0)
+            break;
+        i = 0;
+        A = (int *) malloc(count * sizeof(int));
+        while (i < count) {
+            scanf("%d", &A[i]);
+            i++;
+        }
+        count = maxSubArray(A, 9);
+        printf("%d\n", count);
+    }
     return 0;
 }
